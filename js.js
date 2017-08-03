253fb1aadba013c985011b29671897ec98f44e36
Irinafasyura <irinafasyura998@gmail.com>
2017-08-04 02:59:16 +0300

    adding class text-strike to span on checkbox clicked

--- js.js
+++ js.js
@@ -26,15 +26,31 @@ window.onload = function() {
 		var output='';
 		for (var key in todoList) {
 			if (todoList[key] .check ) {
-            output += '<input type="checkbox" checked>'; 
-
+            output += '<input type="checkbox" checked>';   
 			}	
 			else {
 				output += '<input  id="' + key + 'A" type="checkbox">';
 			}
-			    output += '<span>' + todoList[key].todo + '</span><br>'; 
-	
+			output += '<span  id="' + key + 'B">' + todoList[key].todo + '</span><br>';   // key +B
 		}
 		document.getElementById('out').innerHTML = output;
+		for (var key in todoList) {
+			document.getElementById(key + 'A').onclick = function (event) {
+				if(event.target.checked) {
+				    document.getElementById(event.target.id.replace('A','B')).className ="text-strike";
+					// add / remove class text-strike to span element
+				}
+				else {
+				document.getElementById(event.target.id.replace('A','B')).className =" ";
+				}
+
+			}
+		}
 	}
+
+		// for(var key in todoList)
+		//document.getElementById(key + A) subscribe on change  <-- checkbox
+		//document.getElementById(key + B)                      <-- todo item (text)
+		//document.getElementById('out').innerHTML = output;
 }
+ 
